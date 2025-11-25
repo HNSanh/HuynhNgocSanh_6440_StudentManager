c97d886af0d99ac291fff2055012e69e8cd13924
HNSanh <huynhngocsanh@dtu.edu.vn>
2025-11-26 00:24:39 +0700

    UI : add layout and control

--- StudentManager/Form1.Designer.cs
+++ StudentManager/Form1.Designer.cs
@@ -28,13 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtlop = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gender = new System.Windows.Forms.ComboBox();
+            this.board = new System.Windows.Forms.DataGridView();
+            this.add = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.update = new System.Windows.Forms.Button();
+            this.search = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.board)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(145, 89);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(177, 22);
+            this.txtname.TabIndex = 0;
+            this.txtname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtlop
+            // 
+            this.txtlop.Location = new System.Drawing.Point(145, 189);
+            this.txtlop.Name = "txtlop";
+            this.txtlop.Size = new System.Drawing.Size(177, 22);
+            this.txtlop.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // gender
+            // 
+            this.gender.FormattingEnabled = true;
+            this.gender.Items.AddRange(new object[] {
+            "Nam",
+            "Nữ"});
+            this.gender.Location = new System.Drawing.Point(145, 145);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(121, 24);
+            this.gender.TabIndex = 6;
+            this.gender.Text = "Nam/Nữ";
+            // 
+            // board
+            // 
+            this.board.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.board.Location = new System.Drawing.Point(395, 57);
+            this.board.Name = "board";
+            this.board.RowHeadersWidth = 51;
+            this.board.RowTemplate.Height = 24;
+            this.board.Size = new System.Drawing.Size(393, 335);
+            this.board.TabIndex = 7;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(87, 272);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 58);
+            this.add.TabIndex = 8;
+            this.add.Text = "Thêm";
+            this.add.UseVisualStyleBackColor = true;
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(191, 272);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 58);
+            this.delete.TabIndex = 9;
+            this.delete.Text = "Xoá";
+            this.delete.UseVisualStyleBackColor = true;
+            // 
+            // update
+            // 
+            this.update.Location = new System.Drawing.Point(87, 351);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(75, 58);
+            this.update.TabIndex = 10;
+            this.update.Text = "Cap Nhat";
+            this.update.UseVisualStyleBackColor = true;
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(191, 351);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(75, 58);
+            this.search.TabIndex = 11;
+            this.search.Text = "Tim kiem";
+            this.search.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.board);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtlop);
+            this.Controls.Add(this.txtname);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.board)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtlop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox gender;
+        private System.Windows.Forms.DataGridView board;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button search;
     }
 }
 
